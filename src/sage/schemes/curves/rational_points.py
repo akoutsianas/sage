c1a098a91bb1bc939784c48dbab521e2fa26fb6f
Angelos Koutsianas <koutsis.jr@gmail.com>
2023-03-21 11:56:39 +0200

    Initial implementation of lattice reduction method throught the class RationalPointsAffineCurveLatticeReductionRationalField.

--- src/sage/schemes/curves/rational_points.py
+++ src/sage/schemes/curves/rational_points.py
@@ -37,10 +37,13 @@ from sage.rings.rational_field import QQ
 from sage.rings.padics.factory import Qp
 from sage.sets.primes import Primes
 from sage.rings.finite_rings.finite_field_constructor import FiniteField
-from sage.schemes.affine.affine_space import AffineSpace
+from sage.rings.infinity import infinity
 from sage.schemes.projective.projective_space import ProjectiveSpace
 from sage.modules.free_module_element import vector
 from sage.matrix.constructor import matrix
+from sage.functions.log import log
+from sage.misc.functional import round
+from sage.matrix.special import block_matrix, identity_matrix, zero_matrix, diagonal_matrix, block_diagonal_matrix
 from sage.schemes.affine.affine_rational_point import enum_affine_rational_field, enum_affine_number_field
 from sage.schemes.projective.projective_rational_point import enum_projective_rational_field, enum_projective_number_field
 
@@ -90,6 +93,7 @@ class RationalPointsAffineCurve(RationalPoints):
     def __init__(self, C, height_bound, prec=50, **kwargs):
         super().__init__(C, height_bound)
         self.defining_polynomials = list(C.defining_polynomials())
+        self._n = C.ambient_space().dimension()
         self._kwargs = kwargs
         self.prec = prec
 
@@ -110,10 +114,152 @@ class RationalPointsAffineCurve(RationalPoints):
                     prec=self.prec
                 )
                 return rpachrf.get_small_height_points()
+            elif method == 'lattice':
+                if self.basis_ring == QQ:
+                    if 'p' not in self._kwargs.keys():
+                        raise ValueError('There is no given prime p')
+                    rpachrf = RationalPointsAffineCurveLatticeReductionRationalField(
+                        self.C,
+                        self.height_bound,
+                        p=self._kwargs['p'],
+                        prec=self.prec
+                    )
+                    return rpachrf.get_small_height_points()
             else:
                 return []
         else:
             raise NotImplementedError('There is no other method implemented!')
+
+
+class RationalPointsAffineCurveLatticeReductionRationalField(RationalPointsAffineCurve):
+    """
+        Computes the rational points on the affine curve of up to the given height_bound over QQ using lattice
+        reduction method.
+
+        INPUT:
+
+        - ``C`` -  a curve.
+        - ``height_bound`` - a positive integer bound.
+        - ``p`` - a given prime that we use in the hyperplane method.
+        - ``prec`` - the precision of the p-adic numbers
+
+
+        OUTPUT:
+            - a list containing the points of ``X`` of hyperplane height up to ``height_bound`` sorted.
+    """
+
+    def __init__(self, C, height_bound, p, **kwargs):
+        super().__init__(C, height_bound)
+        self.p = p
+        self.prec = self._initialize_prec()
+        self._Fp = FiniteField(p)
+        self._Qp = Qp(p, prec=self.prec)
+        if p not in Primes():
+            raise ValueError('The number p is not a prime!')
+        self.Cp = C.change_ring(FiniteField(p))
+        if not self.Cp.is_smooth():
+            raise ValueError('The reduction curve Cp is not smooth.')
+        self._pts_modp = self.Cp.rational_points()
+        self._Jac = C.Jacobian_matrix()
+        self._kwargs = kwargs
+        self._powers = [p ** i for i in range(2 * self.prec + 1)]
+        self._lattices = None
+
+    def _initialize_prec(self):
+        return round(2 * log(self.height_bound) / log(self.p)) + 1
+
+    def get_points(self):
+        self._lattices = self._initialize_lattices()
+        for k in range(self.prec + 1):
+            self._lift_lattices()
+        pts = []
+        for L in self._lattices:
+            pt = self._get_point_from_lattice(L)
+            if pt and pt not in pts:
+                pts.append(pt)
+        return pts
+
+    def _get_point_from_lattice(self, L):
+        pt = []
+        for M in L['L']:
+            v = M.transpose().LLL()[0]
+            pt.append(v[1] / v[0])
+        for f in self.defining_polynomials:
+            if f(pt) != 0:
+                return None
+        return pt
+
+    def _initialize_lattices(self):
+        lattices = []
+        for barP in self._pts_modp:
+            xk = [ZZ(ai) for ai in barP]
+            J = self._Jac(xk).change_ring(self._Fp)
+            Jac_kernel = [vector(ZZ, list(v)) for v in J.transpose().kernel().list()]
+            L = self._get_lattice_matrix(xk, 1)
+            lattice = {
+                'barP': barP,
+                'xk': xk,
+                'k': 1,
+                'Jac': J,
+                'Jac_kernel': Jac_kernel,
+                'L': L,
+                'L0': None
+            }
+            lattices.append(lattice)
+        return lattices
+
+    def _get_lattice_matrix(self, x, k):
+        D = []
+        for xi in x:
+            A = matrix(ZZ, 2, [1, 0, xi, self._powers[k]])
+            D.append(A)
+        return D
+
+    def _lift_lattices(self):
+        lattices = []
+        for lattice_dict in self._lattices:
+            lift_lattices = self._lift_lattice(lattice_dict)
+            for lift_lattice in lift_lattices:
+                if self._keep_lift_lattice(lift_lattice, lattice_dict):
+                    lattices.append(lift_lattice)
+        print('number of lift lattices: ', len(lattices))
+        self._lattices = lattices
+
+    def _lift_lattice(self, lattice_dict):
+        xk = lattice_dict['xk']
+        k = lattice_dict['k']
+        Jac_kernel = lattice_dict['Jac_kernel']
+        J = lattice_dict['Jac']
+        L = lattice_dict['L']
+        lift_pts = self._lift_point(xk, k, Jac_kernel, J)
+        lattices = []
+        for lift_pt in lift_pts:
+            lift_L = self._get_lattice_matrix(lift_pt, k + 1)
+            lattice = {
+                    'barP': lattice_dict['barP'],
+                    'xk': lift_pt,
+                    'k': k + 1,
+                    'Jac': J,
+                    'Jac_kernel': Jac_kernel,
+                    'L': lift_L,
+                    'L0': L
+                }
+            lattices.append(lattice)
+        return lattices
+
+    def _lift_point(self, xk, k, Jac_kernel, J):
+        b = vector(self._Fp, [-f(xk)/self._powers[k] for f in self.defining_polynomials])
+        sol = J.solve_right(b)
+        sols = [(sol + v).change_ring(ZZ) for v in Jac_kernel]
+        xk1s = [list(vector(xk) + v * self._powers[k]) for v in sols]
+        return xk1s
+
+    def _keep_lift_lattice(self, L1, L0):
+        for i in range(self._n):
+            m1 = L1['L'][i].transpose().LLL()[0]
+            if m1.norm(infinity) > self.height_bound:
+                return False
+        return True
 
 
 class RationalPointsAffineCurveHyperplaneRationalField(RationalPointsAffineCurve):
@@ -142,7 +288,7 @@ class RationalPointsAffineCurveHyperplaneRationalField(RationalPointsAffineCurve
         self.Cp = C.change_ring(FiniteField(p))
         if not self.Cp.is_smooth():
             raise ValueError('The reduction curve Cp is not smooth.')
-        self.pts_modp = self.Cp.rational_points()
+        self._pts_modp = self.Cp.rational_points()
         self._kwargs = kwargs
         self._powers = [p**i for i in range(prec+2)]
 
